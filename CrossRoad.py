69ea4f89cea305bc3426c64f6cb89b63f7104e9c
Josh R <jriver35@uncc.edu>
2020-08-06 15:17:42 -0400

    added images to directory and added player blit

--- CrossRoad.py
+++ CrossRoad.py
@@ -18,9 +18,12 @@ is_game_over = False
 
 # Create display screen window
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# Set background color
+# Set screen stuff
 game_screen.fill(WHITE_COLOR)
 pygame.display.set_caption(SCREEN_TITLE)
+
+player_image = pygame.image.load("player.png")
+player_image = pygame.transform.scale(player_image, (50, 50))
 
 while not is_game_over:
 
@@ -28,6 +31,11 @@ while not is_game_over:
         if event.type == pygame.QUIT:
             is_game_over = True
         print(event)
+
+    game_screen.blit(player_image, (375, 375))
+
+    # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
+    # pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
     
     # Update display and clock tick
     pygame.display.update()
