9e2d1f1f7f4bdd857c08f5456f03ff7582c6bb93
Josh R <jriver35@uncc.edu>
2020-08-06 14:49:45 -0400

    Initial setup

---
+++ CrossRoad.py
@@ -0,0 +1,9 @@
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+WHITE_COLOR = (255, 255, 255)
+BLACK_COLOR = (0, 0, 0)
+
+game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+game_screen.fill(WHITE_COLOR)
