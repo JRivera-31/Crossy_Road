ba5a4a12a2c536eb3020e2939b1f9fd7a080dba6
Josh R <jriver35@uncc.edu>
2020-08-06 15:01:20 -0400

    changed screent title

--- CrossRoad.py
+++ CrossRoad.py
@@ -4,9 +4,10 @@ import pygame
 # Init pygame
 pygame.init()
 
-# Screen size
+# Screen stuff
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Crossy RPG"
 # Colors in rgb codes
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
@@ -19,13 +20,15 @@ is_game_over = False
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # Set background color
 game_screen.fill(WHITE_COLOR)
+pygame.display.set_caption(SCREEN_TITLE)
 
 while not is_game_over:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_game_over = True
-
+        print(event)
+    
     # Update display and clock tick
     pygame.display.update()
     clock.tick(TICK_RATE)
