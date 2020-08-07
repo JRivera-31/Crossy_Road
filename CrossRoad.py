c1ce92072378fb48d8391661b84ac9fe087f0283
Josh R <jriver35@uncc.edu>
2020-08-06 23:52:38 -0400

    Added collision detection

--- CrossRoad.py
+++ CrossRoad.py
@@ -25,13 +25,15 @@ class Game:
         self.game_screen = pygame.display.set_mode((width, height))
         # Set screen stuff
         self.game_screen.fill(WHITE_COLOR)
-        pygame.display.set_caption(SCREEN_TITLE)
+        pygame.display.set_caption(title)
 
     def run_game_loop(self):
         is_game_over = False
         direction = 0
 
+        treasure = GameObject("treasure.png", 375, 50, 50, 50)
         player_character = PlayerCharacter("player.png", 375, 700, 50, 50)
+        enemy_0 = EnemyCharacter("enemy.png", 20, 400, 50,50)
 
         while not is_game_over:
 
@@ -53,17 +55,22 @@ class Game:
                 print(event)
 
             self.game_screen.fill(WHITE_COLOR)
+            # Draw treasure
+            treasure.draw(self.game_screen)
 
             # Update player position
-            player_character.move(direction)
+            player_character.move(direction, self.height)
+
+            enemy_0.move(self.width)
             # Draw player
             player_character.draw(self.game_screen)
-
-            # game_screen.blit(player_image, (375, 375))
-
-            # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
-            # pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+            enemy_0.draw(self.game_screen)
             
+            if player_character.detect_collision(enemy_0):
+                is_game_over = True
+            elif player_character.detect_collision(treasure):
+                is_game_over = True
+
             # Update display and clock tick
             pygame.display.update()
             clock.tick(self.TICK_RATE)
@@ -90,12 +97,43 @@ class PlayerCharacter(GameObject):
         super().__init__(image_path, x, y, width, height)
 
     # move character up and down
-    def move(self, direction):
+    def move(self, direction, max_height):
         if direction > 0:
             self.y_pos -= self.SPEED
         elif direction < 0:
             self.y_pos += self.SPEED
+        
+        # Make sure character never goes passed bottom of the screen
+        if self.y_pos >= max_height - 40:
+            self.y_pos = max_height - 40
+        
+    def detect_collision(self, other_body):
+        # Check if overlap in y position
+        if self.y_pos > other_body.y_pos + other_body.height:
+            return False
+        elif self.y_pos + self.height < other_body.y_pos:
+            return False
+        # Check if overlap in x position
+        if self.x_pos > other_body.x_pos + other_body.width:
+            return False
+        elif self.x_pos + self.width < other_body.x_pos:
+            return False
+        return True
 
+
+class EnemyCharacter(GameObject):
+    SPEED = 5
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # move character up and down
+    def move(self, max_width):
+        if self.x_pos <= 20:
+            self.SPEED = abs(self.SPEED)
+        elif self.x_pos >= max_width - 40:
+            self.SPEED = -abs(self.SPEED)
+        self.x_pos += self.SPEED
 
 # Init pygame
 pygame.init()
